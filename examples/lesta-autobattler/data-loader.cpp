b5eed36415fa298cc6ac95457f98ee7aa8205e25
broscillograph <kvkmai@mail.ru>
2025-09-30 15:18:54 +0300

    fast save: preparing to implement serialization mechanisms

---
+++ examples/lesta-autobattler/data-loader.cpp
@@ -0,0 +1,34 @@
+#include <lesta-autobattler/data-loader.h>
+
+#include <yaml_cpp/include/yaml-cpp/yaml.h>
+
+namespace LAutobattler
+{
+    GameConfig LoadConfig(const std::string& filename)
+    {
+        // prepare data structure
+        GameConfig config;
+        
+        // open file and read its data
+        
+        // parse data
+        
+        // edit prepared data structure
+        
+        // return the results
+    }
+    
+    HighScoresDB LoadHighScores(const std::string filename)
+    {
+        // prepare data structure
+        HighScoresDB db;
+        
+        // open file and read its data
+        
+        // parse data
+        
+        // edit prepared data structure
+        
+        // return the results
+    }
+}
---
+++ examples/lesta-autobattler/data-loader.h
@@ -0,0 +1,113 @@
+#ifndef LESTA_AUTOBATTLER_DATA_LOADER_H
+#define LESTA_AUTOBATTLER_DATA_LOADER_H
+
+#include <lesta-autobattler/game-fwd.h>
+#include <yaml-cpp/yaml.h>
+
+namespace YAML 
+{
+    // custom types
+//    template<>
+//    struct convert<glm::vec3> 
+//    {
+//        static Node encode(const glm::vec3& rhs) 
+//        {
+//            Node node;
+//            node.push_back(rhs.x);
+//            node.push_back(rhs.y);
+//            node.push_back(rhs.z);
+//            return node;
+//        }
+//        
+//        static bool decode(const Node& node, glm::vec3& rhs) 
+//        {
+//            if (!node.IsSequence() || (node.size() != 3))
+//            {
+//                return false;
+//            }
+//            rhs.x = node[0].as<float>();
+//            rhs.y = node[1].as<float>();
+//            rhs.z = node[2].as<float>();
+//            return true;
+//        }
+//    };
+//    
+//    template<>
+//    struct convert<glm::vec4> 
+//    {
+//        static Node encode(const glm::vec4& rhs) 
+//        {
+//            Node node;
+//            node.push_back(rhs.x);
+//            node.push_back(rhs.y);
+//            node.push_back(rhs.z);
+//            node.push_back(rhs.w);
+//            return node;
+//        }
+//        
+//        static bool decode(const Node& node, glm::vec4& rhs) 
+//        {
+//            if (!node.IsSequence() || (node.size() != 4))
+//            {
+//                return false;
+//            }
+//            rhs.x = node[0].as<float>();
+//            rhs.y = node[1].as<float>();
+//            rhs.z = node[2].as<float>();
+//            rhs.w = node[3].as<float>();
+//            return true;
+//        }
+//    };
+}
+
+
+namespace LAutobattler
+{
+    /* ========================================================================================== */
+    /*                                        TYPES                                               */
+    /* ========================================================================================== */
+    struct GameConfig
+    {
+        bool music = false;
+        int musicVolume = 0;
+        bool sounds = false;
+        int soundsVolume = 0;
+    };
+    
+    struct HighScore
+    {
+        std::u32string nick = U"";
+        Class characterClass = {Classes::None, 1};
+        Weapon weapon;
+        int score = 0;
+    };
+    
+    struct HighScoresDB
+    {
+        std::vector<HighScore> data;
+    };
+    
+    /* ========================================================================================== */
+    /*                                 YAML ENCODE/DECODE                                         */
+    /* ========================================================================================== */
+    YAML::Emitter& operator<<(YAML::Emitter& out, const GameConfig& config)
+    {
+        out << YAML::Flow;
+        out << YAML::BeginSeq;
+        
+        out << config.music;
+        out << config.musicVolume;
+        out << config.sounds;
+        out << config.soundsVolume;
+        
+        out << YAML::EndSeq;
+        return out;
+    }
+    
+    
+    
+    GameConfig LoadConfig(const std::string& filename);
+    HighScoresDB LoadHighScores(const std::string filename);
+}
+
+#endif
